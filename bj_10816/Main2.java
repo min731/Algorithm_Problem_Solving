808b14f51ce65e033b02ff83f627b49721cc3291
min0731 <ypd04208@gmail.com>
2023-01-17 10:07:27 +0900

    Solving_01_17

--- bj_10816/Main2.java
+++ bj_10816/Main2.java
@@ -2,36 +2,29 @@ package bj_10816;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {    
+public class Main2 {    
     public static void main(String[] args) {
 
         Scanner stdIn = new Scanner(System.in);
 
-        // int N = stdIn.nextInt();
-        // int [] cardArray = new int[N];
+        int N = stdIn.nextInt();
+        int [] cardArray = new int[N];
 
-        int [] cardArray = new int[] {3,7,-2,5,5};
+       for (int i=0;i<N;i++){
 
-        Arrays.sort(cardArray);
-        System.out.println(Arrays.toString(cardArray));
+             cardArray[i] = stdIn.nextInt();
 
-        // [-10, -10, 2, 3, 3, 3, 3, 5, 6, 7, 7, 10, 10, 10, 10]
-
-    //   for (int i=0;i<cardArray.length;i++){
-
-    //         cardArray[i] = stdIn.nextInt();
-
-    //     }
-
-        // int M = stdIn.nextInt();
-        // int [] findArray = new int[M];
-        // for (int i=0;i<findArray.length;i++){
-
-        //     findArray[i] = stdIn.nextInt();
-
-        // }
+         }
         
-        int [] findArray = new int [] {7,9,-5,5,3,0,6};
+        Arrays.sort(cardArray);
+
+         int M = stdIn.nextInt();
+         int [] findArray = new int[M];
+         for (int i=0;i<M;i++){
+
+             findArray[i] = stdIn.nextInt();
+
+         }
 
         for (int i : binary_search(cardArray, findArray)){
 
@@ -52,28 +45,22 @@ public class Main {
             int pr = cardArray.length-1;   
 
             while(findArray[i]!=cardArray[pc]){
-                System.out.println("findArray[i]: " + findArray[i] + " cardArray[pc]: " + cardArray[pc]);
-                System.out.println("pl: "+pl + " pc: " + pc + " pr: " + pr);
+
                 if(findArray[i]>cardArray[pc]){
                     pl = pc+1;
                     pc += (pr-pl)/2+1;
                 }
-                else{ // cardArray[j] > findArray[i]
+                else{ 
                     pr = pc-1;
-
                     pc = pc-(pr-pl)/2-1;
-
-                    //   5-(4-0)/2-1 
                 }
-                System.out.println("pl: "+pl + " pc: " + pc + " pr: " + pr);
 
                 if(pl > pc || pc > pr){
                     break;
                 }
 
-
             }
-            
+
             if ( 0<=pc && pc < cardArray.length && findArray[i]==cardArray[pc]){
                 output[i] +=1;
                 for (int m=pc-1; 0<=m;m--){
@@ -87,7 +74,6 @@ public class Main {
                     }
                 }
             }
-            System.out.println(Arrays.toString(output));
             
         }
 
