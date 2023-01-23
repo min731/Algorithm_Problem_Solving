883b264951e30cbcef13627660bb0881b8bb34f4
min0731 <ypd04208@gmail.com>
2023-01-23 10:41:06 +0900

    Solving_01_23

--- bj_1302/Correct_answer.java
+++ bj_1302/Correct_answer.java
@@ -8,8 +8,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Try {
+public class Correct_answer {
 
+    // 입력값 받기
     static HashMap<String, Integer> getData() throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,22 +30,24 @@ public class Try {
             map1.put(book, map1.get(book) + 1);
 
         }
-        System.out.println(map1);
         return map1;
     }
 
+    // 베스트 셀러 출력하기
     static String getAns(HashMap<String, Integer> map1) {
 
         String best = "";
         int max = 0;
         ArrayList<String> bests = new ArrayList<>();
 
+        // 가장 큰 value값 찾기
         for (String key : map1.keySet()) {
             if (map1.get(key) >= max) {
                 max = map1.get(key);
             }
         }
 
+        // 베스트 셀러 이름 수집
         for (Map.Entry<String, Integer> entry1 : map1.entrySet()) {
             String key = entry1.getKey();
             Integer value = entry1.getValue();
@@ -53,10 +56,9 @@ public class Try {
             }
         }
 
+        // 베스트셀러 이름 정렬
         Collections.sort(bests);
-        System.out.println(bests);
         best = bests.get(0);
-
         System.out.println(best);
 
         return best;
@@ -68,7 +70,10 @@ public class Try {
         HashMap<String, Integer> map1 = new HashMap<>();
         String best = "";
 
+        // 입력값 받기
         map1 = getData();
+
+        // 베스트셀러 출력
         getAns(map1);
 
     }
