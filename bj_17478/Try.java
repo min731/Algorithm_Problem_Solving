20a60bb8664296c1465c9f3d9474e01e37fde83b
min0731 <ypd04208@gmail.com>
2023-01-24 23:06:03 +0900

    Solving_01_24

---
+++ bj_17478/Try.java
@@ -0,0 +1,46 @@
+package bj_17478;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Try {
+
+    static BufferedWriter recur(int n, int m, BufferedWriter bw) throws IOException {
+
+        bw.write("_".repeat(4 * (m - n)) + "\"재귀함수가 뭔가요?\"\n");
+
+        if (n == 0) {
+
+            bw.write("_".repeat(4 * (m - n)) + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+        } else {
+            bw.write("_".repeat(4 * (m - n)) + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+            bw.write("_".repeat(4 * (m - n)) + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+            bw.write("_".repeat(4 * (m - n)) + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+
+            recur(n - 1, m, bw);
+
+        }
+
+        bw.write("_".repeat(4 * (m - n)) + "라고 답변하였지.\n");
+
+        return bw;
+
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = n;
+
+        bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+        bw = recur(n, m, bw);
+        bw.flush();
+
+    }
+
+}
