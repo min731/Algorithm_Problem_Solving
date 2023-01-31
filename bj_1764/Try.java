ed98257797b19f460075fed580c2209f9b750b4b
min0731 <ypd04208@gmail.com>
2023-01-31 16:26:49 +0900

    Solving_01_31

---
+++ bj_1764/Try.java
@@ -0,0 +1,25 @@
+package bj_1764;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Try {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        LinkedList<String> be_heard = new LinkedList<>();
+        LinkedList<String> be_seen = new LinkedList<>();
+
+        for()
+        
+    }
+    
+}
