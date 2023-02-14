7d4f4695456dc162f3af11b65c35b2db33f17881
min0731 <ypd04208@gmail.com>
2023-02-14 23:37:18 +0900

    Solving_02_14

---
+++ bj_11582/Try.java
@@ -0,0 +1,33 @@
+package bj_11582;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Try {
+
+    static int [] buff;
+
+    static void merge1
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+        int [] a = new int [n];
+
+        for(int i;i<n;i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        
+    }
+    
+}
