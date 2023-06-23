4a69f1c5748aa8caebd62df947416d7d7fdd68d0
min0731 <ypd04208@gmail.com>
2023-06-23 23:28:18 +0900

    Solving_06_23

---
+++ prg_체육복/Try3.py
@@ -0,0 +1,47 @@
+def solution(n, lost, reserve):
+
+    answer = 0
+
+    items = [1 for i in range(n)]
+
+    print(items)
+
+    for i in lost:
+        items[i-1] = 0
+    
+    print(items)
+
+    for i in reserve:
+        items[i-1] += 1
+    print(items)
+
+    if items[0] == 2 and items[1] == 0:
+        items[0], items[1] = 1,1
+
+    for i in range(1,len(items)-1):
+        if items[i] == 2:
+            if items[i-1] == 0:
+                items[i] = 1
+                items[i-1] = 1
+            elif items[i+1] == 0:
+                items[i] = 1
+                items[i+1] = 1
+
+
+    print(items)
+
+    for x in items:
+        if x != 0:
+            answer += 1
+
+    print(answer)
+
+    return answer
+
+# solution(5,[2, 4],[1, 3, 5])
+# solution(5,[2, 4],[3])
+# solution(3,[3],[1])
+# solution(5,[3,4],[3])
+solution(5,[],[3])
+
+
