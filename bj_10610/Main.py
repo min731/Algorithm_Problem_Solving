3165d6c6aa609b047a4be7e1022c8814c647602b
min731 <ypd04208@gmail.com>
2023-06-28 09:09:31 +0900

    Solving_06_28

---
+++ bj_10610/Main.py
@@ -0,0 +1,22 @@
+N = str(input())
+
+ans = -1
+nums = list(N)
+
+# 정렬했기 때문에 0이 있다면 이미 맨끝
+nums = sorted(nums,reverse=True)
+
+# 30의 배수
+# 3의 배수 : 모든 수의 합이 3의 배수
+# 10의 배수 : 끝자리가 0
+if '0' not in nums:
+    print(-1)
+else:
+    n_sum = 0 
+    for i in range(len(nums)):
+        n_sum += int(nums[i])
+
+    if n_sum%3 == 0:
+        print("".join(nums))
+    else:
+        print(-1)
---
+++ bj_10610/Try.py
@@ -0,0 +1,20 @@
+N = str(input())
+
+ans = -1
+nums = list(N)
+nums = sorted(nums,reverse=True)
+length = len(nums)
+
+while length>0 :
+
+    comb = 0
+    for i in range(length):
+
+
+
+
+
+    if comb%3 == 0:
+        ans = comb 
+        break
+    length -= 1
