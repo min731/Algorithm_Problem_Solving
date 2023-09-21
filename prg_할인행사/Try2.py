a181b014588559711c735e0297b0f6b7b0af24ee
min731 <ypd04208@gmail.com>
2023-09-21 18:58:08 +0900

    Solving_09_21

---
+++ prg_할인행사/Try2.py
@@ -0,0 +1,32 @@
+def solution(want, number, discount):
+    from collections import deque,Counter
+    
+    answer = 0
+
+    want_n = {w:n for w,n in zip(want,number)}
+    # print(want_n)
+
+    discount_queue = deque(discount)
+
+    while len(discount_queue)>=10:
+        print("------------------------")
+        print("discount_queue : ",discount_queue)
+        discount_counter = dict(Counter(list(discount_queue)[:10]))
+
+        if discount_counter == want_n:
+            answer += 1
+
+        print("want_n : ",want_n)
+        print("discount_counter : ",discount_counter)
+
+        discount_queue.popleft()
+
+    print(answer)
+    return answer
+
+# solution(["banana", "apple", "rice", "pork", "pot"],
+#          [3, 2, 2, 2, 1,],
+#          ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"])
+solution(["apple"],
+         [10],
+         ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"])
---
+++ prg_할인행사/Try7.py
@@ -0,0 +1,45 @@
+def solution(want, number, discount):
+    from collections import deque,Counter
+    
+    answer = 0
+
+    want_n = Counter({w:n for w,n in zip(want,number)})
+    # print(want_n)
+
+    discount_queue = deque(discount)
+
+    while len(discount_queue)>=10:
+        
+        is_answer = True
+        print("------------------------")
+        print("discount_queue : ",discount_queue)
+        discount_counter = Counter(list(discount_queue)[:10])
+
+        for k in discount_counter.keys():
+            try :
+                if discount_counter[k] == want_n[k]:
+                    continue
+                else:
+                    is_answer = False
+                    break
+            except:
+                is_answer = False
+                break
+        # print("want_n : ",want_n)
+        # print("discount_counter : ",discount_counter)
+
+        if is_answer:
+            print("want_n : ",want_n)
+            print("discount_counter : ",discount_counter)
+            answer += 1
+        discount_queue.popleft()
+
+    print(answer)
+    return answer
+
+solution(["banana", "apple", "rice", "pork", "pot"],
+         [3, 2, 2, 2, 1,],
+         ["chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"])
+# solution(["apple"],
+#          [10],
+#          ["banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"])
