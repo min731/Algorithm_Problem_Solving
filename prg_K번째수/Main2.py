7baf88f85d70508aa6b2c88c0f3a0a704c6cfaf5
min731 <ypd04208@gmail.com>
2023-10-30 17:50:29 +0900

    Solving_10_30

---
+++ prg_K번째수/Main2.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i,j,k = command
+        tmp = list(array[i-1:j])
+        tmp.sort()
+        answer.append(tmp[k-1])
+        
+    return answer
