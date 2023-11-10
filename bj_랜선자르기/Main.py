eabaa19a5fe1836170049265abafc37205e63d23
min731 <ypd04208@gmail.com>
2023-11-10 18:13:38 +0900

    Solving_11_10

--- bj_랜선자르기/Main.py
+++ bj_랜선자르기/Main.py
@@ -1,9 +1,12 @@
 def binary_search(lines,N):
-    start = 0
+    start = 1
     end = lines[0]
+    ans_list = []
 
     if N <= sum([line//end for line in lines]):
-        return end 
+        # return end 
+        print(end)
+        return
 
     # cnt = 0
     while start<=end:
@@ -15,25 +18,19 @@ def binary_search(lines,N):
         #     return 0
         mid = (start+end)//2
 
-        print("------------------")
-        print("start : ",start)
-        print("mid : ",mid)
-        print("end : ",end)
-
         for line in lines:
             cnt += line//mid
-            print("line//lengths[mid] : ",line//mid)
-        print("cnt : ",cnt)
 
         if cnt >= N:
-            print("cnt : ",cnt)
-            print(" K : ",N)
-            return mid
+            start = mid+1
+            ans_list.append(mid)
         
         # if cnt > N:
         #     start = mid+1
         if cnt < N:
             end = mid-1
+
+    return print(max(ans_list))
 
 K,N = map(int,input().split())
 
@@ -44,18 +41,5 @@ for _ in range(K):
     lines.append(int(input()))
 # print(lines)
 lines.sort(reverse=True)
-print(lines)
 
-ans = binary_search(lines,N)
-print(ans)
-
-# 3 7
-# 51
-# 31
-# 21
-
-# 4 4
-# 10
-# 10
-# 10
-# 10
+binary_search(lines,N)
--- bj_랜선자르기/Try3.py
+++ bj_랜선자르기/Try3.py
@@ -1,9 +1,12 @@
 def binary_search(lines,N):
     start = 0
     end = lines[0]
+    ans_list = []
 
     if N <= sum([line//end for line in lines]):
-        return end 
+        # return end 
+        print(end)
+        return
 
     # cnt = 0
     while start<=end:
@@ -28,12 +31,15 @@ def binary_search(lines,N):
         if cnt >= N:
             print("cnt : ",cnt)
             print(" K : ",N)
-            return mid
+            start = mid+1
+            ans_list.append(mid)
         
         # if cnt > N:
         #     start = mid+1
         if cnt < N:
             end = mid-1
+
+    return print(max(ans_list))
 
 K,N = map(int,input().split())
 
@@ -46,8 +52,7 @@ for _ in range(K):
 lines.sort(reverse=True)
 print(lines)
 
-ans = binary_search(lines,N)
-print(ans)
+binary_search(lines,N)
 
 # 3 7
 # 51
@@ -58,4 +63,7 @@ print(ans)
 # 10
 # 10
 # 10
+# 10
+
+# 1 1
 # 10
