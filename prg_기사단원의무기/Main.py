659c3dc9052104f4407e7179414c12b44a9881d5
min731 <ypd04208@gmail.com>
2023-11-28 16:49:25 +0900

    Solving_11_28

---
+++ prg_기사단원의무기/Main.py
@@ -0,0 +1,22 @@
+def get_divisor(num):
+    
+    divisors = 0
+    
+    for i in range(1,num+1):
+        if num%i==0:
+            divisors += 1
+    
+    return divisors
+
+def solution(number, limit, power):
+    answer = 0
+    
+    for i in range(1,number+1):
+        divisors = get_divisor(i)
+        
+        if divisors>limit:
+            answer += power
+        else:
+            answer += divisors
+    
+    return answer
---
+++ prg_기사단원의무기/Try2.py
@@ -0,0 +1,27 @@
+def get_divisor(num,limit,power):
+    
+    divisors = 0
+    
+    for i in range(1,num+1):
+        if num%i==0:
+            divisors += 1
+        if divisors > limit:
+            divisors = power
+            break
+    
+    return divisors
+
+def solution(number, limit, power):
+    
+    answer = 0
+    
+    for i in range(1,number+1):
+                    
+        divisors = get_divisor(i,limit,power)
+        
+        if divisors>limit:
+            answer += power
+        else:
+            answer += divisors
+    
+    return answer
