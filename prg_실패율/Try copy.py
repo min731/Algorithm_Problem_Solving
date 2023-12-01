e0087d62ee5512f628a1c24bbe8da47020d731fe
min731 <ypd04208@gmail.com>
2023-12-01 18:50:42 +0900

    Solving_12_01

---
+++ prg_실패율/Try copy.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+def solution(N, stages):
+    answer = []
+    
+    players = len(stages)
+    
+    stages = Counter(stages)
+    print(stages)
+    print(stages.most_common())
+    
+    res = {str(i+1):0 for i in range(N)}
+    # res = Counter(res)
+
+    for value,cnt in stages.most_common():
+        res[str(value)] = (players-cnt)/cnt
+        #answer.append(value/players)
+
+    #answer.sort()
+    #print(answer)
+    
+    res = Counter(res)
+    print(res.most_common())
+    
+    return res 
