b06577ff68342e8041182a430910df344252bed2
min731 <ypd04208@gmail.com>
2023-12-10 16:15:28 +0900

    Solving_12_10_2nd

--- prg_완주하지못한선수/Main2.py
+++ prg_완주하지못한선수/Main2.py
@@ -2,4 +2,9 @@ import collections
 
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
+
+    print(answer)
     return list(answer.keys())[0]
+
+
+solution(["leo", "kiki", "eden"],["eden", "kiki"])
