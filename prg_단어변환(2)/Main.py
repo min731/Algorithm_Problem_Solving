dd34e7575fb05985ffa26daaee422ae670ef43a3
min0731 <ypd04208@gmail.com>
2024-01-04 10:53:05 +0900

    Solving_01_04

---
+++ prg_단어변환(2)/Main.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    
+    queue = deque([[0,begin]])
+    
+    while queue:
+        
+        cnt, now = queue.popleft()
+        
+        if now == target:
+            answer = cnt
+            break
+            
+        for word in words:
+            diff = 0
+            for w1,w2 in zip(now,word):
+                if w1!=w2:
+                    diff += 1
+            if diff == 1:
+                queue.append([cnt+1,word])
+            
+    return answer
