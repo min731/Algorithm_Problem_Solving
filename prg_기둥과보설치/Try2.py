f36977f080ef00b22b4cb393d94661ed9e4a005d
min0731 <ypd04208@gmail.com>
2024-01-15 15:09:31 +0900

    Solving_01_15

---
+++ prg_기둥과보설치/Try2.py
@@ -0,0 +1,61 @@
+def solution(n, build_frame):
+    answer = []
+    
+    wall = [[-1 for j in range(n+1)] for i in range(n+1)] # -1:빈공간,0:기둥,1:보 
+    
+    for frame in build_frame:
+        x, y, beam, install = frame
+        
+        if install: # 설치
+            now = wall[x][y]
+            
+            if not beam: # 기둥
+                        
+                check_l = wall[x-1][y]
+                check_d = wall[x][y-1]
+                
+                if (y==0 and now==-1) or (check_d==0)or(check_l==1):
+                    wall[x][y] = 0
+                    print(x,y,"기둥 설치!")
+
+            else: # 보
+                check_d = wall[x][y-1]
+                check_r_d = wall[x+1][y-1]
+                check_l = wall[x-1][y]
+                check_r = wall[x+1][y]
+                
+                if (now==0 or check_r==0) or (check_l==1 and check_r==1):
+                    wall[x][y] = 1
+                    print(x,y,"보 설치!")
+                    
+        else:
+            now = wall[x][y]
+            if not beam: # 기둥
+                check_u = wall[x][y]
+                check_u_l = wall[x-1][y+1]
+                check_u_r = wall[x+1][y+1]
+                
+                if not (check_u==1 and check_u_l==1 and check_u_r==1):
+                    wall[x][y] = -1
+                    # print(x,y,"기둥 제거!")
+                    
+            else: # 보
+                check_l = wall[x-1][y]
+                check_r = wall[x+1][y]
+                
+                if not (now==1 and check_l==1 and check_r==1):
+                    wall[x][y] = -1
+                    # print(x,y,"보 제거!")
+                    
+                    
+    # print(wall)
+    
+    for i in range(n+1):
+        for j in range(n+1):
+            v = wall[i][j]
+            if v != -1:
+                answer.append([i,j,v])
+        
+    answer.sort()
+    # print(answer)
+    return answer
