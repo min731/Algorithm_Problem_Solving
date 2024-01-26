ac62ee2232cfa6ea9d4fb890f612e52ee719e582
min0731 <ypd04208@gmail.com>
2024-01-26 15:08:31 +0900

    Solving_01_26

---
+++ bj_설탕배달/Try.py
@@ -0,0 +1,32 @@
+N = int(input())
+ans = 0
+find = False
+
+if N%5==0:
+    ans = N//5
+    find = True
+else:
+    bongji5 = N//5+1 # 5kg 봉지 갯수
+    # bongji3 = 0 # 3kg 봉지 갯수
+
+    while bongji5>=0:
+
+        # print("N : ", N, "bongji5 : ",bongji5)
+        rest = N-bongji5*5
+        print("N : ", N, "bongji5 : ",bongji5, "rest : ",rest)
+
+        
+        if rest<0:
+            bongji5 -= 1
+        elif rest>0:
+            if rest%3==0:
+                ans = bongji5+rest//3
+                find = True
+                break
+            else:
+                bongji5 -= 1
+
+if find:
+    print(ans)
+else:
+    print(-1)
